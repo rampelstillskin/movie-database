41f6fe761d1ef46ff2a1b07224deedbcdfccc069
Andrey Kharitonov <rampelstillskin@gmail.com>
2022-04-06 11:57:37 +0300

    update file

--- app/js/card.js
+++ app/js/card.js
@@ -1,7 +1,7 @@
 const tabWrapper = document.querySelector(".tab__wrapper");
 
 tabWrapper.addEventListener("click", (event) => {
-    let { target } = event;
+    const { target } = event;
     const films = document.querySelector(".tab__film");
     const bookmarks = document.querySelector(".tab__bookmark");
 
@@ -25,27 +25,19 @@ tabWrapper.addEventListener("click", (event) => {
 // СОЗДАНИЕ КАРТОЧЕК С ТЕГАМИ
 
 const movieTagList = document.querySelector(".movie-tag__list");
-const tagRequest = fetch("../tags.json");
 
-tagRequest
-    .then((response) => response.json())
-    .then((tagList) => {
-        const tagCard = tagList.map(
-            (tag) => `<li class="movie-tag__item">
-				<span class="movie-tag__title">${tag}</span>
-			</li>`
-        );
-        movieTagList.innerHTML = tagCard.join("");
-    });
+const tagRequest = fetch("../../tags").then((response) => response.json());
+
+const tagCard = tagRequest.map((tag) => `<li class="movie-tag__item"><span class="movie-tag__title">${tag}</span></li>`);
+
+movieTagList.innerHTML = tagCard.join("");
 
 // РЕАЛИЗАЦИЯ ПОИСКА ПО НАЗВАНИЮ
 
 const movieSearch = document.querySelector(".movie-search");
 
-const getMovieTitle = async (searchMovie) => {
-    const movieListRequest = await fetch("../films.json").then((response) =>
-        response.json()
-    );
+const getMovieTitle = (searchMovie) => {
+    const movieListRequest = fetch("../../films.json");
 
     let movieMatch = movieListRequest.filter((movie) => {
         const regex = new RegExp(`^${searchMovie}`, "gi");
@@ -67,11 +59,11 @@ movieSearch.addEventListener("input", () => {
 
 const tagFilter = (movie) => {
     const movieCards = document.querySelectorAll(".movie-card");
-    const movieCardList = document.querySelector(".movie-card__list");
+    const movieCardList = document.querySelector(".movie-cards__list");
     const movieTag = document.querySelector(".movie-tag__item");
 
     const regMovie = new RegExp(
-        `(?:^|[^a-zA-Zа-яА-ЯёЁ])(?:${movie.textContent})(?![a-zA-Zа-яА-ЯёЁ])`
+        `(?:^|[^a-zA-Zа-яА-ЯёЁ])(?:${movie.textContent})(?![a-zA-Zа-яА-ЯёЁ])`,
     );
 
     if (movieCards.length !== 0) {
@@ -98,6 +90,49 @@ const removeMovieLocalStorage = (movie) => {
     localStorage.removeItem(movie);
 };
 
+// СОЗДАНИЕ КАРТОЧЕК C ФИЛЬМАМИ
+
+const movieCardList = document.querySelector(".movie-cards__list");
+
+const makeCardMovie = (movies) => {
+    const movieCardHtml = movies.map((card) => {
+        const activeStarOrNot = localStorage.getItem(card.title)
+            ? "active-star"
+            : "not-active-star";
+
+        return `<li class="movie-card">
+            <span class="movie-card__title" data-tag="${card.tags}">${card.title}</span>
+            <span class="movie-card__img" data-img="${activeStarOrNot}"></span>
+        </li>`;
+    });
+
+    movieCardList.innerHTML = movieCardHtml.join("");
+};
+
+// СОЗДАНИЕ И УДАЛЕНИЕ КАРТОЧЕК В ЗАКЛАДКАХ
+
+const bookmarkList = document.querySelector(".bookmark__list");
+
+const makeBookmarkCard = () => {
+    const bookmarkMovieList = [];
+    const keys = Object.keys(localStorage);
+
+    for (const key of keys) {
+        const bookmarkItem = `<li class="movie-card">
+            <span class="movie-card__title">${key}</span>
+            <span class="movie-card__img" data-img="active-star"></span>
+            </li>`;
+
+        bookmarkMovieList.push(bookmarkItem);
+    }
+
+    bookmarkList.innerHTML = bookmarkMovieList.join("");
+};
+
+const deleteBookMarkCard = () => {
+    makeBookmarkCard();
+};
+
 // СОЗДАНИЕ/УДАЛЕНИЕ КАРТОЧЕК И ДОБАВЛЕНИЕ/УДАЛЕНИЕ КЛАССА
 
 tabWrapper.addEventListener("click", (event) => {
@@ -121,45 +156,3 @@ tabWrapper.addEventListener("click", (event) => {
         makeBookmarkCard();
     }
 });
-
-// СОЗДАНИЕ КАРТОЧЕК C ФИЛЬМАМИ
-
-const movieCardList = document.querySelector(".movie-card__list");
-
-export const makeCardMovie = (movies) => {
-    const movieCardHtml = movies.map((card) => {
-        let activeStarOrNot = localStorage.getItem(card.title)
-            ? "active-star"
-            : "not-active-star";
-
-        return `<li class="movie-card">
-						<span class="movie-card__title" data-tag="${card.tags}">${card.title}</span>
-						<span class="movie-card__img" data-img="${activeStarOrNot}"></span>
-				</li>`;
-    });
-
-    movieCardList.innerHTML = movieCardHtml.join("");
-};
-
-// СОЗДАНИЕ И УДАЛЕНИЕ КАРТОЧЕК В ЗАКЛАДКАХ
-
-const bookmarkList = document.querySelector(".bookmark__list");
-
-const makeBookmarkCard = () => {
-    let bookmarkMovieList = [];
-    let keys = Object.keys(localStorage);
-
-    for (let key of keys) {
-        let bookmarkItem = `<li class="movie-card">
-				<span class="movie-card__title">${key}</span>
-				<span class="movie-card__img" data-img="active-star"></span>`;
-
-        bookmarkMovieList.push(bookmarkItem);
-    }
-
-    bookmarkList.innerHTML = bookmarkMovieList.join("");
-};
-
-const deleteBookMarkCard = () => {
-    makeBookmarkCard();
-};
